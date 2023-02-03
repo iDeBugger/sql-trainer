fa84ccd0acd72482863ad30772ec673cab618799
iDeBugger Freeman <me@idebugger.io>
2023-02-03 23:30:46 +0100

    Complete header layout

--- src/blocks/Header/Header.tsx
+++ src/blocks/Header/Header.tsx
@@ -1,5 +1,4 @@
-import { ReactNode } from "react";
-import { useFocusRing } from "react-aria";
+import { Key, ReactNode } from "react";
 import { useTranslation } from "react-i18next";
 import { Item } from "react-stately";
 import { CloseIcon } from "../../assets/icons/CloseIcon";
@@ -14,16 +13,32 @@ import { ModalButton } from "../../components/ModalButton/ModalButton";
 import { Select } from "../../components/Select/Select";
 import { LanguageType } from "../../store/reducers/settingsReducer";
 
-interface HeaderLayoutProps {
+interface LayoutProps {
   children: ReactNode;
 }
 
-interface HeaderRowProps {
+interface RowProps {
   children: ReactNode;
+  className?: string;
 }
 
-interface HeaderLogoRowProps {
+interface LogoRowProps {
   children: ReactNode;
+  className?: string;
+}
+
+interface LanguageSelectProps {
+  selectedLanguage: LanguageType;
+  onSelect: (new_lang: LanguageType) => void;
+}
+
+interface ThemeButtonProps {
+  onClick: () => void;
+}
+
+interface SupportUsButtonProps {
+  onClick: () => void;
+  fill?: Parameters<typeof Button>[0]["fill"];
 }
 
 export interface HeaderProps {
@@ -44,27 +59,88 @@ const LANGUAGES: { title: string; value: LanguageType }[] = [
   },
 ];
 
-function HeaderLayout({ children }: HeaderLayoutProps) {
-  return <div className="container px-6 py-2 flex">{children}</div>;
-}
-
-function HeaderRow({ children }: HeaderRowProps) {
+function Layout({ children }: LayoutProps) {
   return (
-    <div className="container py-2 flex flex-row justify-between">
+    <div className="w-full lg:container px-6 py-2 flex flex-col">
       {children}
     </div>
   );
 }
 
-function HeaderLogoRow({ children }: HeaderLogoRowProps) {
+function Row({ children, className }: RowProps) {
   return (
-    <HeaderRow>
+    <div
+      className={`w-full lg:container py-2 flex flex-row justify-between ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+function LogoRow({ children, className }: LogoRowProps) {
+  return (
+    <Row className={className}>
       <div className="flex flex-row gap-2 justify-start items-center">
         <LogoIcon className="w-10 h-10 flex-shrink-0" />
         <LogoText className="w-[126px] h-[40px] flex-shrink-0 text-gray-900 dark:text-gray-50" />
       </div>
       {children}
-    </HeaderRow>
+    </Row>
+  );
+}
+
+function LanguageSelect({
+  selectedLanguage,
+  onSelect: onLanguageSelect,
+}: LanguageSelectProps) {
+  const { t } = useTranslation();
+
+  const languageItems = LANGUAGES.map(({ title, value }) => (
+    <Item key={value}>{t(title)}</Item>
+  ));
+
+  return (
+    <Select
+      name="language"
+      label=""
+      aria-label="Language select"
+      selectedKey={selectedLanguage}
+      onSelectionChange={(selectedKey: Key) =>
+        onLanguageSelect(selectedKey as LanguageType)
+      }
+    >
+      {languageItems}
+    </Select>
+  );
+}
+
+function ThemeToggler({ onClick }: ThemeButtonProps) {
+  const { t } = useTranslation();
+
+  return (
+    <Button
+      leftIcon={<SunIcon />}
+      variant="secondary"
+      size="medium"
+      aria-label={t("toggle_color_scheme") || undefined}
+      onPress={onClick}
+    />
+  );
+}
+
+function SupportMeButton({ onClick, fill }: SupportUsButtonProps) {
+  const { t } = useTranslation();
+
+  return (
+    <Button
+      leftIcon={<HeartOutlineIcon />}
+      variant="primary"
+      size="medium"
+      fill={fill}
+      onPress={onClick}
+    >
+      {t("support_me")}
+    </Button>
   );
 }
 
@@ -78,13 +154,9 @@ export function Header(props: HeaderProps) {
     onSupportMeClick,
   } = props;
 
-  const languageItems = LANGUAGES.map(({ title, value }) => (
-    <Item key={value}>{t(title)}</Item>
-  ));
-
   return (
-    <HeaderLayout>
-      <HeaderLogoRow>
+    <Layout>
+      <LogoRow className="sm:hidden">
         <ModalButton
           buttonProps={{
             leftIcon: <MoreHorizontalIcon />,
@@ -97,47 +169,81 @@ export function Header(props: HeaderProps) {
         >
           {(onCloseClick) => (
             <Dialog>
-              <HeaderLayout>
-                <HeaderLogoRow>
+              <Layout>
+                <LogoRow>
                   <Button
                     variant="tertiary"
                     onPress={onCloseClick}
                     leftIcon={<CloseIcon />}
                   />
-                </HeaderLogoRow>
-              </HeaderLayout>
+                </LogoRow>
+                <Row>
+                  <LanguageSelect
+                    selectedLanguage={selectedLanguage}
+                    onSelect={onLanguageSelect}
+                  />
+                  <ThemeToggler onClick={onThemeButtonClick} />
+                </Row>
+                <Row>
+                  <SupportMeButton
+                    fill="fillContainer"
+                    onClick={onSupportMeClick}
+                  />
+                </Row>
+              </Layout>
             </Dialog>
           )}
         </ModalButton>
-      </HeaderLogoRow>
-      {/* <div className="hidden lg:flex flex-row gap-3 justify-end">
-        <Select
-          name="language"
-          label=""
-          aria-label="Language select"
-          selectedKey={selectedLanguage}
-          onSelectionChange={(selectedKey: Key) =>
-            onLanguageSelect(selectedKey as LanguageType)
-          }
-        >
-          {languageItems}
-        </Select>
-        <Button
-          leftIcon={<SunIcon />}
-          variant="secondary"
-          size="medium"
-          aria-label={t("toggle_color_scheme") || undefined}
-          onPress={onThemeButtonClick}
-        />
-        <Button
-          leftIcon={<HeartOutlineIcon />}
-          variant="primary"
-          size="medium"
-          onPress={onSupportMeClick}
-        >
-          {t("support_me")}
-        </Button>
-      </div> */}
-    </HeaderLayout>
+      </LogoRow>
+      <LogoRow className="hidden sm:max-md:flex">
+        <div className="flex flex-row gap-3">
+          <SupportMeButton onClick={onSupportMeClick} />
+          <ModalButton
+            buttonProps={{
+              leftIcon: <MoreHorizontalIcon />,
+              variant: "tertiary",
+              size: "medium",
+              "aria-label": t("open_main_menu") || undefined,
+            }}
+            isDismissable
+            position="topFullWidth"
+          >
+            {(onCloseClick) => (
+              <Dialog>
+                <Layout>
+                  <LogoRow>
+                    <div className="flex flex-row gap-3">
+                      <SupportMeButton onClick={onSupportMeClick} />
+                      <Button
+                        variant="tertiary"
+                        onPress={onCloseClick}
+                        leftIcon={<CloseIcon />}
+                      />
+                    </div>
+                  </LogoRow>
+                  <Row>
+                    <LanguageSelect
+                      selectedLanguage={selectedLanguage}
+                      onSelect={onLanguageSelect}
+                    />
+                    <ThemeToggler onClick={onThemeButtonClick} />
+                  </Row>
+                </Layout>
+              </Dialog>
+            )}
+          </ModalButton>
+        </div>
+      </LogoRow>
+      <LogoRow className="hidden md:flex">
+        <div className="flex flex-row gap-3">
+          <LanguageSelect
+            selectedLanguage={selectedLanguage}
+            onSelect={onLanguageSelect}
+          />
+          <ThemeToggler onClick={onThemeButtonClick} />
+          <SupportMeButton onClick={onSupportMeClick} />
+        </div>
+      </LogoRow>
+    </Layout>
   );
 }
