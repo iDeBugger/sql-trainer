f770e16b96e16f2e5c70985db4cbcef552c3ff5a
iDeBugger Freeman <me@idebugger.io>
2023-02-15 10:15:00 +0100

    Fix build

--- src/store/thunks/checkAnswerThunk.ts
+++ src/store/thunks/checkAnswerThunk.ts
@@ -1,40 +1,6 @@
-import { tasksList } from "../../assets/tasks/tasks";
 import { createAppAsyncThunk } from "../store";
-import {
-  setDbStatus,
-  setSelectedTask,
-  setTables,
-} from "../reducers/taskReducer";
-import { databases } from "../../assets/databases/databases";
 
 export const selectTask = createAppAsyncThunk(
   "tasks/checkAnswer",
-  async (solutionQuery: string, { dispatch }) => {
-    dispatch(setDbStatus("INITIALIZING"));
-    let selectedTask = tasksList.find(({ id }) => id === newTaskId);
-
-    if (!selectedTask) {
-      selectedTask = tasksList[0];
-      console.warn(
-        `Selected task (${newTaskId}) hadn't been found in tasks list. First task (${selectedTask.id}) was selected instead.`
-      );
-    }
-    dispatch(setSelectedTask(selectedTask.id));
-
-    const database = databases[selectedTask.database];
-    console.time("Database (re)initialization");
-    const dbWorkerInstance = await dbWorker;
-    await dbWorkerInstance.initDb(database);
-    console.timeEnd("Database (re)initialization");
-
-    console.time("Tables data extraction");
-    const tables = await dbWorkerInstance.getTablesDescription(
-      selectedTask.tables
-    );
-    console.timeEnd("Tables data extraction");
-
-    dispatch(setTables(tables));
-
-    dispatch(setDbStatus("READY"));
-  }
+  async (solutionQuery: string, { dispatch }) => {}
 );
