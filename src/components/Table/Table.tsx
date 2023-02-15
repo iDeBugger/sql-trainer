ec12df14fa09f8baf6c861eb8238075b87604cf1
iDeBugger Freeman <me@idebugger.io>
2023-02-15 19:44:05 +0100

    Fix clip of ForeignKey tooltip

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -10,27 +10,26 @@ interface TableProps {
 }
 
 const styleToOuterDivClass: { [_ in TableStyle]: string } = {
-  default:
-    "overflow-clip border-l-4 border-l-bluealpha-100 dark:border-l-blue-500",
+  default: "border-l-4 border-l-bluealpha-100 dark:border-l-blue-500",
   zebra: "",
 };
 
 const styleToInnerDivClass: { [_ in TableStyle]: string } = {
   default:
-    "overflow-clip border-r border-y border-gray-200 dark:border-gray-700 rounded-tr-xl rounded-br-xl",
+    "border-r border-y border-gray-200 dark:border-gray-700 rounded-tr-xl rounded-br-xl",
   zebra: "",
 };
 
 const styleToHeaderCellClass: { [_ in TableStyle]: string } = {
   default:
-    "px-[18px] py-[12px] border-gray-200 text-gray-1000 bg-gray-0 dark:border-gray-700 dark:text-gray-100 dark:bg-gray-800",
+    "px-[18px] py-[12px] rounded-t-xl border-gray-200 text-gray-1000 bg-gray-0 dark:border-gray-700 dark:text-gray-100 dark:bg-gray-800",
   zebra:
     "p-4 border-gray-200 text-gray-900 bg-bluealpha-8 dark:border-gray-700 dark:text-gray-100 dark:bg-bluealpha-24",
 };
 
 const styleToBodyCellClass: { [_ in TableStyle]: string } = {
   default:
-    "px-[18px] py-[10px] border-gray-200 dark:border-gray-700 text-gray-900 dark:text-gray-100 bg-gray-0 dark:bg-gray-800",
+    "px-[18px] py-[10px] last:rounded-b-xl border-gray-200 dark:border-gray-700 text-gray-900 dark:text-gray-100 bg-gray-0 dark:bg-gray-800",
   zebra:
     "px-4 py-3 border-gray-200 dark:border-gray-700 text-gray-900 dark:text-gray-100 bg-gray-0 dark:bg-gray-900 group-even/row:bg-gray-50 dark:group-even/row:bg-gray-800",
 };
